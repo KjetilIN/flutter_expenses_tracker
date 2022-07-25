451fd87e7509998434b3d1ea092a18f5c097b09a
KjetilLaptop <kjetikin@stud.ntnu.no>
2022-07-25 18:41:54 +0200

    Refactor to widgets + add new transaction logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_expenses_tracker/transaction.dart';
+import '../widgets/user_transactions.dart';
 
-import 'package:intl/intl.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,20 +15,10 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-  final List<Transaction> transactions = [
-    Transaction(
-      id: "24fd",
-      title: "New Shoes",
-      amount: 102,
-      date: DateTime.now(),
-    ),
-    Transaction(
-      id: "3ddfs",
-      title: "Basketball",
-      amount: 12,
-      date: DateTime.now(),
-    ),
-  ];
+
+
+  String? titleInput;
+  String? amoutInput;
 
   @override
   Widget build(BuildContext context) {
@@ -47,66 +36,7 @@ class MyHomePage extends StatelessWidget {
             ),
             elevation: 5,
           ),
-          Card(
-            child: Container(
-              margin: EdgeInsets.all(10),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                  TextField(
-                    decoration: InputDecoration(labelText: "Title"),
-                  ),
-                  TextField(
-                    decoration: InputDecoration(labelText: "Amount"),
-                  ),
-                  FlatButton(
-                      onPressed: () {},
-                      child: Text(
-                        "Add transaction",
-                        style: TextStyle(color: Colors.purple),
-                      ))
-                ],
-              ),
-            ),
-          ),
-          Column(
-            children: transactions.map((tx) {
-              return Card(
-                  child: Row(
-                children: [
-                  Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                          border: Border.all(color: Colors.purple, width: 2)),
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      child: Text(
-                        "\$${tx.amount}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: Colors.purple),
-                      )),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                          fontWeight: FontWeight.bold,
-                          fontSize: 16,
-                        ),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: TextStyle(color: Colors.grey, fontSize: 13),
-                      ),
-                    ],
-                  ),
-                ],
-              ));
-            }).toList(),
-          )
+          UserTransaction(),
         ],
       ),
     );
